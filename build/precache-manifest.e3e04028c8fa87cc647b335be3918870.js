0120ba8d3267f9f52a522c98d8b8595af21181bb
stefangrujic <gruicstefan@gmail.com>
2020-10-06 19:36:20 -0400

    every thing

---
+++ build/precache-manifest.e3e04028c8fa87cc647b335be3918870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16f23ed48a78ac840c35",
+    "url": "/static/css/main.f946f646.chunk.css"
+  },
+  {
+    "revision": "16f23ed48a78ac840c35",
+    "url": "/static/js/main.16f23ed4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "76f1edf2bb9ad70576cdc7acb02d1bb9",
+    "url": "/index.html"
+  }
+];
